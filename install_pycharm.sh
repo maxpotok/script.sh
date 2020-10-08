4d2bc084d7ca695f752663cc7706d1f9311946ef
Maksymilian Potok <makspotok@gmail.com>
2020-10-08 16:49:15 +0200

    removed hardcoding

--- install_pycharm.sh
+++ install_pycharm.sh
@@ -5,20 +5,23 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-echo "Downloading package"
-wget https://download.jetbrains.com/python/pycharm-community-2020.2.2.tar.gz
+version="2020.2.2"
+url="https://download.jetbrains.com/python/pycharm-community-$version.tar.gz"
 
-echo "Extracting PyCharm"
-tar -xzf pycharm-community-2020.2.2.tar.gz
+echo "Downloading package"
+wget $url
 
 echo "Moving Pycharm folder to /opt directory"
-mv pycharm-community-2020.2.2/ /opt/
+mv pycharm-community-$version.tar.gz/ /opt/
 
 echo "Entering opt/"
 cd /opt/
 
+echo "Extracting PyCharm"
+tar -xzf pycharm-community-$version.tar.gz
+
 echo "Entering Pycharm folder"
-cd pycharm-community-2020.2.2/
+cd pycharm-community-$version/
 
 echo "Entering bin"
 cd bin
